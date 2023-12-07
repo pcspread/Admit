6fd7f098add091d48284efc1c641954c96acbf5f
pcspread <drummer0125vocal@sea.chukai.ne.jp>
2023-12-07 15:57:16 +0900

    created migration

--- database/migrations/2023_12_07_150800_create_notices_table.php
+++ database/migrations/2023_12_07_150800_create_notices_table.php
@@ -11,15 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('notices', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            // $table->rememberToken();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title', 50);
+            $table->string('content');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('notices');
     }
 };
