380b32e28dcc9368995de34b2aeaf56ef97edf3c
pcspread <drummer0125vocal@sea.chukai.ne.jp>
2023-12-08 17:54:57 +0900

    created auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,22 +8,39 @@
 
 @section('content')
 <div class="register-section">
-    <form class="register-form">
+    <form class="register-form" action="/register" method="POST">
+    @csrf
         <div class="register-item">
             <input class="register-input" type="text" name="name" value="" placeholder="氏名" autofocus>
-            <p class="register-error"></p>
+            <p class="register-error">
+            @error('name')
+                {{ $errors->first('name') }}
+            @enderror
+            </p>
         </div>
         <div class="register-item">
             <input class="register-input" type="text" name="email" value="" placeholder="メールアドレス">
-            <p class="register-error"></p>
+            <p class="register-error">
+            @error('email')
+                {{ $errors->first('email') }}
+            @enderror
+            </p>
         </div>
         <div class="register-item">
-            <input class="register-input" type="text" name="password" value="" placeholder="パスワード">
-            <p class="register-error"></p>
+            <input class="register-input" type="password" name="password" value="" placeholder="パスワード">
+            <p class="register-error">
+            @error('password')
+                {{ $errors->first('password') }}
+            @enderror                
+            </p>
         </div>
         <div class="register-item">
-            <input class="register-input" type="text" name="password_confirmation" value="" placeholder="確認用パスワード">
-            <p class="register-error"></p>
+            <input class="register-input" type="password" name="password_confirmation" value="" placeholder="確認用パスワード">
+            <p class="register-error">
+            @error('password_confirmation')
+                {{ $errors->first('password_confirmation') }}
+            @enderror
+            </p>
         </div>
         <button class="register-button">新規登録する</button>
         <a class="register-click" href="/login">ログインはこちら</a>
