e2a1d9015b1d96c685a7e5daab3fc351f3ea63b6
pcspread <drummer0125vocal@sea.chukai.ne.jp>
2023-12-19 11:54:07 +0900

    created report

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -10,7 +10,7 @@ class Report extends Model
     use HasFactory;
 
     protected $fillable = [
-        'user_id', 'date_at', 'content',
+        'user_id', 'date_at', 'content', 'check',
     ];
 
     /**
--- resources/views/report/list.blade.php
+++ resources/views/report/list.blade.php
@@ -14,11 +14,14 @@
         <a class="next-date" href="/report/list?date={{ $addDay->toDateString() }}">></a>
     </div>
     <table class="list-table">
+        @if (count($reports) > 0)
         <tr class="table-item">
             <th class="table-title">番号</th>
             <th class="table-title">氏名</th>
             <th class="table-title"></th>
+            <th class="table-title"></th>
         </tr>
+        @endif
         @foreach ($reports as $report)
         <tr class="table-item">
             <td class="table-content">{{ $report['id'] }}</td>
@@ -26,8 +29,23 @@
             <td class="table-content">
                 <a class="table-content__link" href="/report/detail/{{ $report['id'] }}">詳細</a>
             </td>
+            <td class="table-content">
+                <form class="table-content__form" action="/report/list/{{ $report['id'] }}" method="POST">
+                @csrf
+                    @if (!$report['check'])
+                    <button class="table-content__form-button">未確認</button>
+                    @else
+                    <button class="table-content__form-button checked" type="button">確認済</button>
+                    @endif
+                </form>
+            </td>
         </tr>
         @endforeach
+        @if (count($reports) === 0)
+        <tr class="table-item">
+            <td class="table-content">日報情報がございません</td>
+        </tr>
+        @endif
     </table>
 </div>
 @endsection
